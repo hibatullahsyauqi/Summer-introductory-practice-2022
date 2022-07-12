6c27b99fd10b4384084ba5f7bc8b0b40fe32e458
hibatullahsyauqi <hibatullah_syauqi@hotmail.com>
2022-07-12 16:21:07 +0300

    first commit for proj3.py

--- proj2.py
+++ proj2.py
@@ -1,3 +1,26 @@
+#from math import *
+
+#a = 0
+#b = 1
+#def f(x):
+#    return exp(x)
+#def integrall(a, b):
+#    return exp(b)-exp(a)
+#def integral(a, b, h):
+#    s = 0.0
+#    while a <= b:
+#        s += f(a)*h
+#        a += h
+#    return s
+
+#for i in range(1,8):
+#    h = float(0.1 ** i)
+#    print('\nДлина отрезка', round(h, 8))
+#    print('Приближенный интеграл', integral(a, b, h))
+#    print('Интеграл для f(x) = exp(x)   ->     F(x) = exp(x): ', integrall(a, b))
+#    print('Погрешность: ', abs(integral(a, b, h)-integrall(a, b)))
+#    i += 1
+
 from math import *
 
 a = 0
@@ -9,13 +32,15 @@ def integrall(a, b):
 def integral(a, b, h):
     s = 0.0
     while a <= b:
-        s += f(a)*h
+        def mid(a, h):
+            return (2 * a + h) / 2
+        s += f(mid(a, h))*h
         a += h
     return s
 
-for i in range(1,8):
+for i in range(1,7):
     h = float(0.1 ** i)
-    print('\nДлина отрезка', round(h, 8))
+    print('\nДлина отрезка', round(h, 7))
     print('Приближенный интеграл', integral(a, b, h))
     print('Интеграл для f(x) = exp(x)   ->     F(x) = exp(x): ', integrall(a, b))
     print('Погрешность: ', abs(integral(a, b, h)-integrall(a, b)))
---
+++ proj3.py
@@ -0,0 +1,46 @@
+from math import *
+
+
+def func(x):
+    return cos(x)
+
+def diff(x, h, func):  # вычисление производной методом центральной конечной разности
+    return (func(x + h) - func(x - h)) / (2 * h)
+
+def runge(epsilon, func, h, x):  # вычисление производной с оценкой погрешности правилом рунге
+    h1 = h
+    print("начальный шаг = %.20f" % (h1))
+    h2 = h1 / 2
+    print("начальный половинный шаг = %.20f" % (h2))
+    dif1 = (func(x + h1) - func(x - h1)) / (2 * h)
+    dif2 = (func(x + h2) - func(x - h2)) / (2 * h)
+    differential = abs(dif1 - dif2)
+    err = differential / 3
+    print("ошибка = %.20f" % (err))
+    while err > epsilon:
+        h1 = h2
+        h2 = h1 / 2
+        dif1 = (func(x + h1) - func(x - h1)) / (2 * h)
+        dif2 = (func(x + h2) - func(x - h2)) / (2 * h)
+        differential = abs(dif1 - dif2)
+        if err < differential / 3:
+            print("запрошенная точность недостижима")
+            print("предельная точность при шаге равном == %.20f" % (h2 * 2))
+            print("%24.18f - соотвествующее значение производной" % (err))
+            return 0
+        else:
+            err = differential / 3
+    print("%.18f - шаг для заданной точности" % (h2))
+    print("%.18f - производная" % (dif2))
+    print("%.18f - ошибка" % (err))
+    return 0
+
+
+print("производная для функции (1+x)")
+print("введите шаг: ")
+a = float(input())
+print("введите допустимая погрешность: ")
+b = float(input())
+print("введите точку: ")
+c = float(input())
+runge(b, func, a, c)
